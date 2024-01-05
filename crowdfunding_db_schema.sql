92698b44cdab889641e25f4c99dd7ebcd75116a0
eferna1 <ericamorales@ericas-mac-mini.lan>
2024-01-04 19:21:40 -0600

    update project repo

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -5,29 +5,34 @@
 -- Modify this code to update the DB schema diagram.
 -- To reset the sample schema, replace everything with
 -- two dots ('..' - without quotes).
+drop table campaign;
+drop table category;
+drop table contact;
+drop table subcategory;
+
 
 CREATE TABLE "campaign" (
     "cf_id" INT   NOT NULL,
     "contact_id" INT   NOT NULL,
     "company_name" VARCHAR   NOT NULL,
-    "description" VARCHAR   NOT NULL,
+    "description" VARCHAR(255)   NOT NULL,
     "goal" DECIMAL   NOT NULL,
     "pledged" DECIMAL   NOT NULL,
     "outcome" VARCHAR   NOT NULL,
     "backers_count" INT   NOT NULL,
-    "country" VARCHAR   NOT NULL,
-    "currency" VARCHAR   NOT NULL,
+    "country" VARCHAR(2)   NOT NULL,
+    "currency" VARCHAR(3)   NOT NULL,
     "launch_date" DATE   NOT NULL,
     "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR   NOT NULL,
-    "subcategory_id" VARCHAR   NOT NULL,
+    "category_id" VARCHAR(5)   NOT NULL,
+    "subcategory_id" VARCHAR(9)   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
 );
 
 CREATE TABLE "category" (
-    "category_id" INT   NOT NULL,
+    "category_id" VARCHAR(5)   NOT NULL,
     "category" VARCHAR   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
@@ -45,7 +50,7 @@ CREATE TABLE "contact" (
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" INT   NOT NULL,
+    "subcategory_id" VARCHAR(9)   NOT NULL,
     "subcategory" VARCHAR   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
